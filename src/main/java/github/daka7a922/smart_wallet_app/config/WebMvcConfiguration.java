a513bc46a1e77b8d42f5b2d0272b44d2a7055b02
Danny P <y.popov1516@gmail.com>
2025-02-21 13:35:09 +0200

    Update WebMvcConfiguration.java

--- src/main/java/github/daka7a922/smart_wallet_app/config/WebMvcConfiguration.java
+++ src/main/java/github/daka7a922/smart_wallet_app/config/WebMvcConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -12,6 +13,7 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableMethodSecurity
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
 
